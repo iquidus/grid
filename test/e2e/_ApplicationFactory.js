fa0a23e555d87a56cc8e713cd780e0984817da29
Everton Fraga <ev@ethereum.org>
2019-04-23 15:33:33 -0400

    test: ApplicationFactory

---
+++ test/e2e/_ApplicationFactory.js
@@ -0,0 +1,33 @@
+import {Application} from 'spectron'
+import * as path from 'path'
+
+class ApplicationFactory {
+
+  static development() {
+    const params = {
+      path: path.resolve('./node_modules/.bin/electron'),
+      env: ['-r', path.resolve('./index.js')]
+    }
+
+    console.log('development() app parameters', params);
+    return new Application(params)
+  }
+
+  static release() {
+    const params = {
+      path: path.resolve('./release/mac/Grid.app/Contents/MacOS/Grid')
+    }
+    console.log('installed() app parameters', params);
+    return new Application(params)
+  }
+
+  static installed() {
+    const params = {
+      path: '/Applications/Grid.app/Contents/MacOS/Grid'
+    }
+    console.log('installed() app parameters', params);
+    return new Application(params)
+  }
+
+}
+export default ApplicationFactory
--- test/e2e/app.test.js
+++ test/e2e/app.test.js
@@ -1,10 +1,10 @@
 import test from 'ava'
-import {Application} from 'spectron'
+import ApplicationFactory from './_ApplicationFactory'
 
 test.beforeEach(async t => {
-  t.context.app = new Application({
-    path: '/Applications/Grid.app/Contents/MacOS/Grid'
-  })
+  t.context.app = ApplicationFactory.release()
+
+  console.log(t.context.app);
   await t.context.app.start()
 })
 
@@ -12,24 +12,12 @@ test.afterEach.always(async t => {
   await t.context.app.stop()
 })
 
-test('basic app', async t => {
+test('start app', async t => {
   const app = t.context.app
   await app.client.waitUntilWindowLoaded(20000)
   await app.client.waitUntilTextExists('button', 'NODES', 5000)
 
   const win = app.browserWindow
-  t.is(await app.client.getWindowCount(), 1)
+  t.is(await app.client.getWindowCount(), 2)
   t.true(await win.isVisible())
-})
-
-test('Should quit when main window is closed', async t => {
-  const app = t.context.app
-  await app.client.waitUntilWindowLoaded(10000)
-
-  console.log('window 1', await app.client.getFocusedWindow())
-})
-
-test('.', async t => {
-  const app = t.context.app
-  await app.client.waitUntilWindowLoaded(10000)
 })
