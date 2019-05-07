22ddbb6bd428e1a03a0ec6eed3ce4c509ccbf546
Everton Fraga <ev@ethereum.org>
2019-05-07 10:16:54 -0400

    ci: use newer node version on mac

--- .circleci/install_node.sh
+++ .circleci/install_node.sh
@@ -2,7 +2,7 @@
 
 if [[ $CIRCLE_JOB == 'build-mac' ]]
 then
-  curl "https://nodejs.org/dist/latest/node-${VERSION:-$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')}.pkg" > "$HOME/Downloads/node-latest.pkg" && sudo installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/"
+  curl "https://nodejs.org/dist/v10.0.0/node-v10.0.0.pkg" > "$HOME/Downloads/node-latest.pkg" && sudo installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/"
   curl https://www.npmjs.com/install.sh | sudo bash
 fi;
 
