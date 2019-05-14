fd26282ec11510a00387a6097e2b8591463f7d7f
philipplgh <philipplgh@gmail.com>
2019-05-14 11:37:00 +0200

    plugin system improvements

--- ethereum_clients/util.js
+++ ethereum_clients/util.js
@@ -3,16 +3,35 @@ const path = require('path')
 const { AppManager } = require('@philipplgh/electron-app-manager')
 
 const getUserDataPath = () => {
-  const USER_DATA_PATH = 'electron' in process.versions
-    ? require('electron').app.getPath('userData')
-    : path.join(process.env.APPDATA, 'grid')
+  const USER_DATA_PATH =
+    'electron' in process.versions
+      ? require('electron').app.getPath('userData')
+      : path.join(process.env.APPDATA, 'grid')
   if (!fs.existsSync(USER_DATA_PATH)) {
     fs.mkdirSync(USER_DATA_PATH)
   }
   return USER_DATA_PATH
 }
 
-const getCachePath = (name) => {
+const getPluginCachePath = name => {
+  let cachePath
+  if (process.env.NODE_ENV === 'test') {
+    cachePath = path.join(__dirname, `client_plugins`, `${name}`)
+  } else if (process.env.NODE_ENV === 'development') {
+    cachePath = path.join(__dirname, `client_plugins`, `${name}`)
+  } else {
+    const USER_DATA_PATH = getUserDataPath()
+    cachePath = path.join(USER_DATA_PATH, `client_plugins`, `${name}`)
+  }
+
+  if (!fs.existsSync(cachePath)) {
+    fs.mkdirSync(cachePath)
+  }
+
+  return cachePath
+}
+
+const getCachePath = name => {
   let cachePath
   if (process.env.NODE_ENV === 'test') {
     cachePath = path.join(__dirname, '/../test', 'fixtures', `bin_${name}`)
@@ -28,14 +47,12 @@ const getCachePath = (name) => {
   }
 
   return cachePath
-
 }
 
 const getBinaryUpdater = (repo, name, filter, prefix, cachePath) => {
-
   let includes = []
   let excludes = []
-  
+
   if (filter && filter.name) {
     const { name } = filter
     excludes = name.excludes
@@ -53,7 +70,10 @@ const getBinaryUpdater = (repo, name, filter, prefix, cachePath) => {
     cacheDir: cachePath,
     filter: ({ fileName }) => {
       fileName = fileName.toLowerCase()
-      return (!includes || includes.every(val => fileName.indexOf(val) >= 0)) && (!excludes || excludes.every(val => fileName.indexOf(val) === -1))
+      return (
+        (!includes || includes.every(val => fileName.indexOf(val) >= 0)) &&
+        (!excludes || excludes.every(val => fileName.indexOf(val) === -1))
+      )
     },
     prefix
   })
@@ -61,5 +81,6 @@ const getBinaryUpdater = (repo, name, filter, prefix, cachePath) => {
 
 module.exports = {
   getCachePath,
+  getPluginCachePath,
   getBinaryUpdater
 }
